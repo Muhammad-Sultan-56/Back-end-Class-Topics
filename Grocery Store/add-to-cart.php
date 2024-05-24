23c1021cdc61ba2704bd40c98dfa4cc2a090c37c
Muhammad Sultan <125191342+Muhammad-Sultan-56@users.noreply.github.com>
2024-05-24 11:47:27 +0500

    Add to cart added

---
+++ Grocery Store/add-to-cart.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once "./includes/db-con.php";
+include_once "./includes/helpers.php";
+
+if (isset($_POST["pid"])) {
+    $pid = $_POST["pid"];
+    $qty = $_POST["qty"];
+    $customer_id = 1;
+
+    // get product details
+    $record = getProducts($con, null, $pid);
+    $product = mysqli_fetch_assoc($record);
+
+    // check this logged in customer has any active order
+
+    $order = getOrderByCustomer($con, 1);
+
+    // if logged in customer don't have pending or means it's his new order
+    if (empty($order)) {
+        // insert new order details
+        createOrder($con, $customer_id, $product, $qty);
+    }
+
+    // if logged in customer has pending/active order
+    if (!empty($order)) {
+        updateOrder($con, $order, $product, $qty);
+    }
+
+
+    $order_items = getOrderItemsByOrderId($con, $order['id']);
+    echo count($order_items);
+}
--- Grocery Store/includes/helpers.php
+++ Grocery Store/includes/helpers.php
@@ -1,17 +1,23 @@
 <?php
 
 
-// get categories from databse
-function getCategories($con)
+function pp($data)
 {
-    $select_category = "SELECT * FROM categories";
-    $select_category_run =  mysqli_query($con, $select_category);
-    return $select_category_run;
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+    //exit;
 }
 
+function getCategories($con)
+{
 
-// get categories by id
-function  getCategroyById($con, $id)
+    $sql = "SELECT * FROM categories";
+    $result = mysqli_query($con, $sql);
+    return $result;
+}
+
+function getCategroyById($con, $id)
 {
     $sql = "SELECT * FROM categories WHERE id = $id";
     $result = mysqli_query($con, $sql);
@@ -19,26 +25,15 @@ function  getCategroyById($con, $id)
     return $categroy;
 }
 
-// function for image path
-function getImageUrl($folder, $image)
-{
-    return "admin/images/$folder/$image";
-}
-
-
-
-// get products from database 
 function getProducts($con, $category = null, $id = null)
 {
-    // get all products
-    $sql = "SELECT * FROM products  ";
 
-    // with id
+    $sql = "SELECT * FROM products ";
+
     if ($category != null) {
         $sql .= "WHERE category = '$category' ";
     }
 
-    // with id and category
     if ($id != null && $category != null) {
         $sql .= "AND id = '$id' ";
     } else if ($id != null && $category == null) {
@@ -51,3 +46,92 @@ function getProducts($con, $category = null, $id = null)
 
     return $result;
 }
+
+
+function getImageUrl($folder, $image)
+{
+    return "admin/images/$folder/$image";
+}
+
+
+// get order of customer
+function getOrderByCustomer($con, $id)
+{
+    $sql = "SELECT * FROM orders WHERE customer_id = $id AND status = 'pending' ";
+    $result = mysqli_query($con, $sql);
+    $order = mysqli_fetch_assoc($result);
+    return $order;
+}
+
+
+// get order all items by order_id
+function getOrderItemsByOrderId($con, $order_id)
+{
+    $sql = "SELECT * FROM order_items WHERE order_id = $order_id";
+    $result = mysqli_query($con, $sql);
+    return mysqli_fetch_all($result);
+}
+
+
+
+// create new order
+function createOrder($con, $customer_id, $product, $qty)
+{
+    $total_price = $product['unit_price'] * $qty;
+
+    $order_sql = "INSERT INTO `orders` (`id`, `customer_id`, `grand_total`, `status`) VALUES (NULL, $customer_id, $total_price, 'pending') ";
+    mysqli_query($con, $order_sql);
+
+    // get last order id
+    $order_id = mysqli_insert_id($con);
+
+    // create products / items
+    $pid = $product['id'];
+    $uprice = $product['unit_price'];
+
+    $item_sql = "INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `unit_price`, `quantity`, `total_price`) VALUES (NULL, $order_id, $pid, $uprice, $qty, $total_price) ";
+    mysqli_query($con, $item_sql);
+
+    return $order_id;
+}
+
+
+// get order item/product by id
+function getOrderItemById($con, $order_id, $pid)
+{
+    $sql = "SELECT * FROM order_items WHERE product_id = $pid AND order_id = $order_id ";
+    $result = mysqli_query($con, $sql);
+    $order_item = mysqli_fetch_assoc($result);
+    return $order_item;
+}
+
+
+
+// update order with products
+function updateOrder($con, $order, $product, $qty)
+{
+    $order_id = $order['id'];
+    $pid = $product['id'];
+    $uprice = $product['unit_price'];
+
+    // check if product is already in cart then update its quantity.
+    $item = getOrderItemById($con, $order_id, $pid);
+    if (empty($item)) {
+        // inserting new item in order
+        $total_price = $product['unit_price'] * $qty;
+        $item_sql = "INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `unit_price`, `quantity`, `total_price`) VALUES (NULL, $order_id, $pid, $uprice, $qty, $total_price) ";
+        mysqli_query($con, $item_sql);
+    } else {
+        // updating existing item quantity
+        $qty_new = $qty + $item['quantity'];
+        $total_price_new = $product['unit_price'] * $qty_new;
+
+        $usql = "UPDATE `order_items` SET `quantity` = $qty_new, `total_price`= $total_price_new WHERE `order_items`.`product_id` = $pid;";
+        mysqli_query($con, $usql);
+    }
+
+
+    return $order_id;
+}
+
+    //
